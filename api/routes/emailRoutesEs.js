77cbbec9da328f20e509006317e7e0701cd13ed6
paniaguaadrian <paniaguasanchezadrian@gmail.com>
2021-06-15 16:38:30 +0200

    1️⃣  V.1.1 🎉 Deleted the PM from the entire flow  🎉

--- api/routes/emailRoutesEs.js
+++ api/routes/emailRoutesEs.js
@@ -2,15 +2,12 @@ import express from "express";
 
 // Controllers imported
 import {
-  sendRJ1FormEmails,
   sendRJ2FormEmails,
   sendRJ11Emails,
   sendRJ12Emails,
-  sendPMEmails,
   sendRJ3FormEmail,
   sendRJ15EmailsTT,
   sendRJ15EmailsPM,
-  sendRJSFormEmail,
   sendRJ3FilesEmail,
   sendRJ18EmailTT,
   sendRJ18EmailPM,
@@ -18,16 +15,13 @@ import {
 
 const router = express.Router();
 
-router.route("/rj1").post(sendRJ1FormEmails);
 router.route("/rj2/tt").post(sendRJ2FormEmails);
 router.route("/rj2/rimbo").post(sendRJ3FilesEmail);
 router.route("/rj11").post(sendRJ11Emails);
 router.route("/rj12").post(sendRJ12Emails);
-router.route("/rjpm").post(sendPMEmails);
 router.route("/rj3").post(sendRJ3FormEmail);
 router.route("/rj15/tt").post(sendRJ15EmailsTT);
 router.route("/rj15/pm").post(sendRJ15EmailsPM);
-router.route("/rjs").post(sendRJSFormEmail);
 router.route("/rj18tt").post(sendRJ18EmailTT);
 router.route("/rj18pm").post(sendRJ18EmailPM);
 
--- client/src/RouterWrapper.js
+++ client/src/RouterWrapper.js
@@ -9,18 +9,14 @@ import { Container } from "@material-ui/core";
 
 // ! Screens
 // Forms
-import RegisterTenancy from "./screens/RegisterTenancy";
 import RegisterTenant from "./screens/RegisterTenant_RJ2/RegisterTenant";
 import RegisterTenantCard from "./screens/RegisterTenantCard_RJ3/StripeHandlerComponent";
-import RegisterTenantPM from "./screens/RegisterTenantPM_RJS/RegisterTenantPM";
 // Approved
 import ApprovedTenantRimbo from "./screens/approvedTenantRimbo/ApprovedTenantRimbo";
-import ApprovedTenancyPM from "./screens/approvedTenancyPM/ApprovedTenancyPM";
 import ApprovedTenantCardRimbo from "./screens/approvedTenantCardRimbo/ApprovedTenantCardRimbo";
 import ApprovedTenancyRimbo from "./screens/approvedTenancyRimbo/ApprovedTenancyRimbo";
 // Rejected
 import RejectedTenantRimbo from "./screens/approvedTenantRimbo/RejectedTenantRimbo";
-import RejectedTenancyPM from "./screens/approvedTenancyPM/RejectedTenancyPM";
 // General
 import Page404 from "./screens/404/404";
 
@@ -29,43 +25,38 @@ const RouterWrapper = () => {
     <>
       <Container maxWidth="xl">
         <Switch>
-          <Route exact path="/register/tenancy" component={RegisterTenancy} />
+          {/* RJ2 */}
           <Route
             exact
             path="/register/tenant/:tenancyID"
             component={RegisterTenant}
           />
+          {/* RJ3 */}
           <Route
             exact
             path="/register/card/:randomID"
             component={RegisterTenantCard}
           />
-          <Route
-            exact
-            path="/register/contract/:tenancyID"
-            component={RegisterTenantPM}
-          />
+
+          {/* Approve by Rimo */}
           <Route
             path="/register/tenant/:tenancyID/approved"
             component={ApprovedTenantRimbo}
           />
+
+          {/* Reject by Rimbo */}
           <Route
             path="/register/tenant/:tenancyID/rejected"
             component={RejectedTenantRimbo}
           />
-          <Route
-            path="/register/tenancy/:tenancyID/pm/approved"
-            component={ApprovedTenancyPM}
-          />
-          <Route
-            path="/register/tenancy/:tenancyID/pm/rejected"
-            component={RejectedTenancyPM}
-          />
+
+          {/* Approve card by Rimbo */}
           <Route
             path="/register/card/:tenancyID/card/approved"
             component={ApprovedTenantCardRimbo}
           />
 
+          {/* Service start by Rimbo */}
           <Route
             path="/register/contract/:tenancyID/service-start"
             component={ApprovedTenancyRimbo}
